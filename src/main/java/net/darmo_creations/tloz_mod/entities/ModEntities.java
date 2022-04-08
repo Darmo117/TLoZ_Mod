281aa1604bcdad0b18718f67ef52665aef03b5bd
Darmo117 <darmo117@gmail.com>
2022-04-08 20:20:36 +0200

    rename entities and tile entities

--- src/main/java/net/darmo_creations/tloz_mod/entities/ModEntities.java
+++ src/main/java/net/darmo_creations/tloz_mod/entities/ModEntities.java
@@ -1,6 +1,7 @@
 package net.darmo_creations.tloz_mod.entities;
 
 import net.darmo_creations.tloz_mod.TLoZ;
+import net.darmo_creations.tloz_mod.items.BossKeyEntity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,48 +13,57 @@ public class ModEntities {
       DeferredRegister.create(ForgeRegistries.ENTITIES, TLoZ.MODID);
 
   public static final RegistryObject<EntityType<BombEntity>> BOMB = REGISTER.register(
-      "bomb_entity",
+      "bomb",
       () -> EntityType.Builder.<BombEntity>create(BombEntity::new, EntityClassification.MISC)
           .size(0.98f, 0.98f)
           .trackingRange(10)
           .updateInterval(10)
           .immuneToFire()
-          .build("bomb_entity")
+          .build("bomb")
   );
   public static final RegistryObject<EntityType<JarEntity>> JAR = REGISTER.register(
-      "jar_entity",
+      "jar",
       () -> EntityType.Builder.<JarEntity>create(JarEntity::new, EntityClassification.MISC)
           .size(0.98f, 0.98f)
           .trackingRange(10)
           .updateInterval(10)
           .immuneToFire()
-          .build("jar_entity")
+          .build("jar")
   );
   public static final RegistryObject<EntityType<ItemBulbEntity>> ITEM_BULB = REGISTER.register(
-      "item_bulb_entity",
+      "item_bulb",
       () -> EntityType.Builder.<ItemBulbEntity>create(ItemBulbEntity::new, EntityClassification.MISC)
           .size(0.98f, 0.98f)
           .trackingRange(10)
           .updateInterval(10)
           .immuneToFire()
-          .build("item_bulb_entity")
+          .build("item_bulb")
   );
   public static final RegistryObject<EntityType<RockEntity>> ROCK = REGISTER.register(
-      "rock_entity",
+      "rock",
       () -> EntityType.Builder.<RockEntity>create(RockEntity::new, EntityClassification.MISC)
           .size(0.98f, 0.98f) // TODO size
           .trackingRange(10)
           .updateInterval(10)
           .immuneToFire()
-          .build("rock_entity")
+          .build("rock")
+  );
+  public static final RegistryObject<EntityType<BossKeyEntity>> BOSS_KEY = REGISTER.register(
+      "boss_key",
+      () -> EntityType.Builder.<BossKeyEntity>create(BossKeyEntity::new, EntityClassification.MISC)
+          .size(0.5f, 0.5f)
+          .trackingRange(10)
+          .updateInterval(10)
+          .immuneToFire()
+          .build("boss_key")
   );
   public static final RegistryObject<EntityType<TLoZArrowEntity>> ARROW = REGISTER.register(
-      "arrow_entity",
+      "arrow",
       () -> EntityType.Builder.<TLoZArrowEntity>create(TLoZArrowEntity::new, EntityClassification.MISC)
           .size(0.5f, 0.5f)
           .trackingRange(10)
           .updateInterval(10)
           .immuneToFire()
-          .build("arrow_entity")
+          .build("arrow")
   );
 }
--- src/main/java/net/darmo_creations/tloz_mod/tile_entities/ModTileEntities.java
+++ src/main/java/net/darmo_creations/tloz_mod/tile_entities/ModTileEntities.java
@@ -13,47 +13,52 @@ public class ModTileEntities {
 
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<BombFlowerTileEntity>> BOMB_FLOWER = REGISTER.register(
-      "bomb_plant_tile_entity",
+      "bomb_plant",
       () -> TileEntityType.Builder.create(BombFlowerTileEntity::new, ModBlocks.BOMB_FLOWER).build(null)
   );
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<ItemBulbFlowerTileEntity>> ITEM_BULB_FLOWER = REGISTER.register(
-      "item_bulb_flower_tile_entity",
+      "item_bulb_flower",
       () -> TileEntityType.Builder.create(ItemBulbFlowerTileEntity::new, ModBlocks.ITEM_BULB_FLOWER).build(null)
   );
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<JarTileEntity>> JAR = REGISTER.register(
-      "jar_tile_entity",
+      "jar",
       () -> TileEntityType.Builder.create(JarTileEntity::new, ModBlocks.JAR).build(null)
   );
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<RockTileEntity>> ROCK = REGISTER.register(
-      "rock_tile_entity",
+      "rock",
       () -> TileEntityType.Builder.create(RockTileEntity::new, ModBlocks.ROCK).build(null)
   );
   @SuppressWarnings("ConstantConditions")
+  public static final RegistryObject<TileEntityType<BossKeyTileEntity>> BOSS_KEY = REGISTER.register(
+      "boss_key",
+      () -> TileEntityType.Builder.create(BossKeyTileEntity::new, ModBlocks.BOSS_KEY).build(null)
+  );
+  @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<BombBreakableBlockTileEntity>> BOMB_BREAKABLE_BLOCK = REGISTER.register(
-      "bomb_breakable_block_tile_entity",
+      "bomb_breakable_block",
       () -> TileEntityType.Builder.create(BombBreakableBlockTileEntity::new, ModBlocks.BOMB_BREAKABLE_BLOCK).build(null)
   );
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<SafeZoneEffectAreaTileEntity>> SAFE_ZONE_EFFECT_AREA = REGISTER.register(
-      "safe_zone_effect_area_tile_entity",
+      "safe_zone_effect_area",
       () -> TileEntityType.Builder.create(SafeZoneEffectAreaTileEntity::new, ModBlocks.SAFE_ZONE_EFFECT_AREA).build(null)
   );
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<SpikesEffectAreaTileEntity>> SPIKES_EFFECT_AREA = REGISTER.register(
-      "spikes_effect_area_tile_entity",
+      "spikes_effect_area",
       () -> TileEntityType.Builder.create(SpikesEffectAreaTileEntity::new, ModBlocks.SPIKES_EFFECT_AREA).build(null)
   );
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<SpawnpointSetterTileEntity>> SPAWNPOINT_SETTER = REGISTER.register(
-      "spawnpoint_setter_tile_entity",
+      "spawnpoint_setter",
       () -> TileEntityType.Builder.create(SpawnpointSetterTileEntity::new, ModBlocks.SPAWNPOINT_SETTER).build(null)
   );
   @SuppressWarnings("ConstantConditions")
   public static final RegistryObject<TileEntityType<TreasureChestTileEntity>> TREASURE_CHEST = REGISTER.register(
-      "treasure_chest_tile_entity",
+      "treasure_chest",
       () -> TileEntityType.Builder.create(TreasureChestTileEntity::new, ModBlocks.TREASURE_CHEST, ModBlocks.DOUBLE_TREASURE_CHEST, ModBlocks.MIMIC_CHEST)
           .build(null)
   );
